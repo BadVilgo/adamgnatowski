128fb77d1793415b3a980047a0ab96476556dc24
Adam <gnatowski.adam.biz@gmail.com>
2024-07-10 11:50:26 +0200

    button-menu-logika

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+// HAMBURGER MENU
+
 const menuWhole = document.querySelector('.menu-whole');
+const menuIcon = document.querySelector('.fa-bars');
+const menuItems = document.querySelectorAll('.menu-whole li');
 
 let toggleMenuDisplay = () => {
-    if (menuWhole.style.display === 'none') {
+    let computedStyle = window.getComputedStyle(menuWhole);
+    if (computedStyle.getPropertyValue('display') === 'none') {
         menuWhole.style.display = 'block';
     } else {
         menuWhole.style.display = 'none';
     }
 }
 
-document.querySelector('.fa-bars').addEventListener('click', toggleMenuDisplay);
-// document.querySelector('section').addEventListener('click', toggleMenuDisplay);
-// document.querySelector('a').addEventListener('click', toggleMenuDisplay);
+menuIcon.addEventListener('click', toggleMenuDisplay);
+
+menuItems.forEach(item => {
+    item.addEventListener('click', toggleMenuDisplay);
+});
+
+document.addEventListener('click', (event) => {
+    if (!menuWhole.contains(event.target) && event.target !== menuIcon) {
+        menuWhole.style.display = 'none';
+    }
+});
+
+// ---------------------------------------------
